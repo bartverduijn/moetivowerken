1750e1ba749c2a1a85408d009a22033b3e8399b4
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2019-04-11 22:06:21 +0200

    pulled link out of component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 import Status from '../components/Status';
 
-const Index = () => <Status />;
+const Index = () => (
+    <>
+        <Status />
+        <Link href="/admin">
+            <a>Sign in</a>
+        </Link>
+    </>
+);
 
 export default Index;
